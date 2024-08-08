67fc3e32ed01a9c258eb29ea458f26704633329b
Pablo Rubiano <pcrubianoa@users.noreply.github.com>
2024-08-08 16:43:34 -0500

    first commit

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API
+});
+
+export const useApi = () => ({
+    validateToken: async (token: string) => {
+        const response = await api.post('/validate', {
+            token
+        })
+        return response.data
+        // return {
+        //     user: { id: 3, name: 'José', email: 'jose@gmail.com' }
+        // };
+    },
+    signin: async (user: string, pass: string, application:string) => {
+        const response = await api.post('https://api.logis.com.co/v1/secure', {
+            user, pass, application
+        })
+        return response.data
+        // return {
+        //     user: { id: 3, name: 'José', email: 'jose@gmail.com' },
+        //     token: '123456789'
+        // };
+    },
+    logout: async () => {
+        const response = await api.post('/logout')
+        return response.data
+        //return { status: true };
+    }
+});
